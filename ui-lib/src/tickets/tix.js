30495ecba4c3bb873ba9987c4d4f9630c3829105
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-07-28 19:27:39 -0600

    Ticket lists

--- ui-lib/src/tickets/tix.js
+++ ui-lib/src/tickets/tix.js
@@ -1,8 +1,35 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+const site = "https://zccmario.zendesk.com/api/v2/requests";
+const token = "rvtGswAyzmWmtUW686a5VjldZDsbhkK0FMiHc02B";
+const pass = "G0a1mark10!";
+const username = "mario-bravo99@hotmail.com";
+const usernamePasswordBuffer = Buffer.from(username + ":" + pass);
+const base64data = usernamePasswordBuffer.toString("base64");
 
 export const LoadTix = () => {
-  const [tickets, getTickets] = useState(["Empty", "sdf"]);
+  const [tickets, getTickets] = useState([]);
+
+  useEffect(() => {
+    const test = async () => {
+      try {
+        const axiosObject = axios.create({
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Basic ${base64data}`,
+          },
+        });
+
+        const res = await axiosObject.get(site);
+        console.log(res);
+        const json = await res.json();
+        console.log(json);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+  }, []);
 
   const load = tickets.map((e) => <li> {e} </li>);
 
