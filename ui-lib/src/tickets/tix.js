1705b3ce797ebe808f95d369ca345cd0cddeebca
mbravofuentes <mbravofuentes@mavs.coloradomesa.edu>
2021-08-03 21:06:01 -0600

    Added message for ticket requests"

--- ui-lib/src/tickets/tix.js
+++ ui-lib/src/tickets/tix.js
@@ -1,20 +1,23 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Row, Col } from "react-bootstrap";
 
 const site = "http://localhost:5000/api";
 
 export const LoadTix = () => {
   const [tickets, setTickets] = useState([]);
+  const [onLoad, setOnLoad] = useState(false);
+
   const response = async () => {
     try {
       const res = await axios.get(site);
       console.log(res);
       const data = [...res.data.tickets];
       setTickets([...data]);
-      console.log(tickets);
+      setOnLoad(true);
     } catch (error) {
       console.log(error);
+      setOnLoad(false);
     }
   };
 
@@ -25,7 +28,7 @@ export const LoadTix = () => {
 
   const goThroughCards = tickets.map((e) => (
     <article key={e.id}>
-      <Card className="text-center">
+      <Card className="text-center" border="primary">
         <Card.Header>{e.subject}</Card.Header>
         <Card.Body>
           <Card.Title>Status: {e.status} </Card.Title>
@@ -34,13 +37,28 @@ export const LoadTix = () => {
         </Card.Body>
         <Card.Footer className="text-muted">
           {getDate(e.created_at)}
+          <Row>
+            <Col>Ticket# - {e.id}</Col>
+          </Row>
         </Card.Footer>
       </Card>
     </article>
   ));
   return (
     <div>
-      <Button onClick={response}> Get Ticket Requests</Button> {goThroughCards}
+      <Row>
+        <Button
+          onClick={!onLoad ? response : null}
+          variant={onLoad ? "success" : "secondary"}
+        >
+          {" "}
+          {onLoad ? "Got Tickets, click to refresh" : "Get Ticket Requests"}
+        </Button>
+      </Row>
+      <hr />
+      <Row xs={1} md={2} className="g-4">
+        {goThroughCards}
+      </Row>
     </div>
   );
 };
